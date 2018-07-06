332d6efd85e473bb7351e41b4c9745e8a80a9dd8
Sunny Gupta <flameheadsg@gmail.com>
2018-07-06 15:13:06 -0700

    added JWT passport authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,43 @@
+const jwt = require('jwt-simple');
+const User = require('../models/User');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user), memo: "successfully signed in" });
+}
+
 exports.signup = function(req, res, next) {
-  res.send({ success: true });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide both an email and a password' });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email already in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.json({ token: tokenForUser(user), memo: "successfully created a new account" });
+    });
+  });
 }
--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send(['waterbottle', 'phone', 'paper']);
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hhho: 'appa' });
   });
 
   app.post('/signup', Authentication.signup);
+
+  app.post('/signin', requireSignin, Authentication.signin);
 }
